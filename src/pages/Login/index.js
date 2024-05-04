190159b31fffff502e452c1512d34bffb39a0cc9
Ricardo Korzekwa <103125283+RicardoKorzekwa@users.noreply.github.com>
2024-05-04 14:18:34 -0300

    loginpt
    Ajustando a tela de login para o caso de precisar cadastrar um usuario montando a estrutura da tela

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,9 +4,33 @@ import { Button, ButtonText, Container, Input, SignUpButton, SignUpText, Title }
 
 export default function Login(){
     const [login, setLogin] = useState(true);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     function toggleLogin(){
         setLogin(!login);
+        setName("");
+        setEmail("");
+        setPassword("");
+    }
+
+    function handleSignIn(){
+        if(email === '' || password === ''){
+            console.log("Preencha todos os campos");
+            return;
+        }
+
+        // Fazer o Login do usuario
+    }
+
+    function handleSignUp(){
+        if(name === '' || email === '' || password === ''){
+            console.log("Preencha todos os campos para cadastrar");
+            return;
+        }
+        
+        //Cadastrar no app
     }
 
     if(login){
@@ -16,10 +40,18 @@ export default function Login(){
                     Dev<Text style={{color: '#E52246'}}>Post</Text>
                 </Title>
     
-                <Input placeholder="seuemail@teste.com"/>
-                <Input placeholder="******"/>
+                <Input 
+                    placeholder="seuemail@teste.com"
+                    value={email}
+                    onChangeText={ (text) => setEmail(text) }
+                />
+                <Input 
+                    placeholder="******"
+                    value={password}
+                    onChangeText={ (text) => setPassword(text) }
+                />
     
-                <Button>
+                <Button onPress={handleSignIn}>
                     <ButtonText>Acessar</ButtonText>
                 </Button>
     
@@ -36,11 +68,25 @@ export default function Login(){
                 Dev<Text style={{color: '#E52246'}}>Post</Text>
             </Title>
 
-            <Input placeholder="Seu Nome"/>
-            <Input placeholder="seuemail@teste.com"/>
-            <Input placeholder="******"/>
+            <Input 
+                placeholder="Seu Nome"
+                value={name}
+                onChangeText={ (text) => setName(text) }
+            />
+            
+            <Input 
+                placeholder="seuemail@teste.com"
+                value={email}
+                onChangeText={ (text) => setEmail(text) }
+            />
+            
+            <Input 
+                placeholder="******"
+                value={password}
+                onChangeText={ (text) => setPassword(text) }
+            />
 
-            <Button>
+            <Button onPress={handleSignUp}>
                 <ButtonText>Cadastrar</ButtonText>
             </Button>
 
